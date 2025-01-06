815c34c82da10a012e7a7f8690f7fa17965f4747
Miroslav Cimerman <mc@doas.su>
2025-01-06 16:06:04 +0100

    more proper initial raid1 create fcn

--- raid1.c
+++ raid1.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+#include "sd.h"
 #include "var.h"
 #include "util.h"
 
@@ -7,11 +10,21 @@ static uint8_t raid1_read(sdvol_t *, uint32_t, void *);
 uint8_t
 raid1_create(sdvol_t *vol)
 {
+	printf("raid1_create()\r\n");
 	vol->dev_ops.vol_write_blk = raid1_write;
 	vol->dev_ops.vol_read_blk = raid1_read;
 	vol->dev_ops.vol_blkno = sdraid_util_get_data_blkno;
 
-	(void)write_metadata(vol);
+	for (uint8_t i = 0; i < vol->devno; i++) {
+		if (sd_init(i) != 0) {
+			printf("initing sd %u failed\r\n", i);
+			return (1);
+		}
+	}
+
+	if (write_metadata(vol) != 0)
+		return (1);
+
 	return (0);
 }
 
