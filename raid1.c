f08cda767e1652de52b6180f765c8a43abd5e4b0
Miroslav Cimerman <mc@doas.su>
2025-01-09 16:25:38 +0100

    RAID1: basic implementation

--- raid1.c
+++ raid1.c
@@ -29,15 +29,60 @@ raid1_create(sdvol_t *vol)
 }
 
 static uint8_t
-raid1_write(sdvol_t *, uint32_t, void *)
+raid1_write(sdvol_t *vol, uint32_t ba, void *data)
 {
 	printf("raid1_write()\r\n");
+
+	if (vol->state == FAULTY)
+		return (1);
+
+	if (ba >= vol->data_blkno)
+		return (2);
+
+	ba += DATA_OFFSET;
+
+	uint8_t succesful = 0;
+	for (uint8_t i = 0; i < vol->devno; i++) {
+		if (vol->extents[i].state != OPTIMAL)
+			continue;
+		if (sd_write(i, ba, data) != 0) {
+			vol->extents[i].state = FAULTY;
+			continue;
+		}
+		succesful++;
+	}
+
+	if (succesful == 0) {
+		vol->state = FAULTY;
+		return (1);
+	}
+
 	return (0);
 }
 
 static uint8_t
-raid1_read(sdvol_t *, uint32_t, void *)
+raid1_read(sdvol_t *vol, uint32_t ba, void *data)
 {
 	printf("raid1_read()\r\n");
-	return (0);
+
+	if (vol->state == FAULTY)
+		return (1);
+
+	if (ba >= vol->data_blkno)
+		return (2);
+
+	ba += DATA_OFFSET;
+
+	for (uint8_t i = 0; i < vol->devno; i++) {
+		if (vol->extents[i].state != OPTIMAL)
+			continue;
+
+		if (sd_read(i, ba, data) == 0)
+			return (0);
+
+		vol->extents[i].state = FAULTY;
+	}
+
+	vol->state = FAULTY;
+	return (1);
 }
